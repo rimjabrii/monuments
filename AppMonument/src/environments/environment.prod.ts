00066003fdff2b3c010eeeb90b65413cc78a273a
Rim Jabri <rimjabri1999@gmail.com>
2021-06-21 20:00:02 +0100

    IONICAPP

---
+++ AppMonument/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDCgM4PLENB7iGbzvFD83OPUFlQdnnDbdE",
+    authDomain: "ionic5-firebase-authentication.firebaseapp.com",
+    databaseURL: "https://ionic5-firebase-authentication.firebaseio.com",
+    projectId: "ionic5-firebase-authentication",
+  }
+};
---
+++ AppMonument/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDCgM4PLENB7iGbzvFD83OPUFlQdnnDbdE",
+    authDomain: "appmonument.firebaseapp.com",
+    projectId: "appmonument",
+    storageBucket: "appmonument.appspot.com",
+    messagingSenderId: "624962190163",
+    appId: "1:624962190163:web:2e25e92dd9b4bff132bfa6",
+    measurementId: "G-C4FBG70Z2Y"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
